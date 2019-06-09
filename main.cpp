3ee209a3dd77a56ea6b5efcb66b0580ec3579fee
stefan <stefan_kote@yahoo.com>
2019-06-09 22:27:39 +0200

    L2: Filled triangle

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 Model *model = NULL;
-const int width  = 800;
-const int height = 800;
+const int width  = 200;
+const int height = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -56,11 +56,45 @@ void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color) {
     }
 }
 
-void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color){
-    line(v0, v1, image, color);
-    line(v0, v2, image, color);
-    line(v1,v2, image, color);
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color){
+    //Checking for outlier triangles
+    if(t0.y == t1.y && t0.y == t2.y)
+        return;
+
+    //Sorting points by height in order do divide triangle in 2 triangles with one horizontal side
+    if (t0.y>t1.y) std::swap(t0, t1);
+    if (t0.y>t2.y) std::swap(t0, t2);
+    if (t1.y>t2.y) std::swap(t1, t2);
+
+    int partTriangleHeight, wholeTriangleHeight = t2.y - t0.y;
+    //Iterating through y values in the triangle like in scan conversion
+    for(int y = 0; y < wholeTriangleHeight; y++){
+        bool secongTriangle = y > t1.y - t0.y || t1.y ==  t0.y;
+        partTriangleHeight = secongTriangle? t2.y - t1.y : t1.y - t0.y; //Checking which part triangle is being drawn ATM
+        //Calculating scalars for determining limiting points in the current horizontal segment
+        float p1 = (float)y / wholeTriangleHeight;
+        float p2 = (float)(y - (secongTriangle? t1.y - t0.y : 0))/ partTriangleHeight;
+
+        //Calculating limiting vectors (vertices) by getting the vector of the 2 barrieres (left and right)
+        //multipying it by the scalars and then moving it in reference to t0 instead of the origin point
+        Vec2i Left = t0 + (t2 - t0) * p1;
+        Vec2i Right = secongTriangle? t1 + (t2 - t1) * p2 : t0 + (t1 - t0) * p2;
+        if(Left.x > Right.x)
+            std::swap(Left, Right);
+
+        //Drawing one horizontal line segment between the limiting points "Left" and "Right"
+        for(int x = Left.x; x <= Right.x; x++){
+            image.set(x, y + t0.y, color);
+        }
+    }
 }
+
+void triangle_outline(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
 
 int main(int argc, char** argv) {
     if (2==argc) {
