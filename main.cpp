e730f61da0c7f423ae05a2683ed5b7b95200fae8
stefan <stefan_kote@yahoo.com>
2019-06-11 14:02:14 +0200

    L2: Added other triangle drawing method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <iostream>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -7,8 +8,8 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 Model *model = NULL;
-const int width  = 200;
-const int height = 200;
+const int width  = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -95,6 +96,44 @@ void triangle_outline(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor co
     line(t2, t0, image, color);
 }
 
+Vec3f cross(Vec3f vect_A, Vec3f vect_B) {
+
+    Vec3f cross_P;
+    cross_P.x = vect_A.y * vect_B.z - vect_A.z * vect_B.y;
+    cross_P.y = vect_A.x * vect_B.z - vect_A.z * vect_B.x;
+    cross_P.z = vect_A.x * vect_B.y - vect_A.y * vect_B.x;
+    return cross_P;
+}
+
+Vec3f barycentric(Vec2i *pts, Vec2i P) {
+    Vec3f u = cross(Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x), Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y));
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+}
+
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
+    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        bboxmin.x = std::max(0,        std::min(bboxmin.x, pts[i].x));
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmin.y = std::max(0,        std::min(bboxmin.y, pts[i].y));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    }
+    Vec2i P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts, P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            image.set(P.x, P.y, color);
+        }
+    }
+}
+
 
 int main(int argc, char** argv) {
     if (2==argc) {
@@ -104,8 +143,24 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0,0,-1);
 
-    triangle(Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80), image, red);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j]  = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        if (intensity>0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+    }
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
